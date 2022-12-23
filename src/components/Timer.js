ba23c8b1aceb5821accac5d1067e9cc32bc970c8
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-12-23 17:25:53 +0100

    run timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,20 +5,21 @@ import {UI_WHITE} from "../constants/uiColors";
 import RangeSlider from 'react-bootstrap-range-slider';
 
 export function Timer() {
-    const {time, setTime} = useTimeContext();
+    const {time, setTime, toggleTimer} = useTimeContext();
     return (
         <>
             <Form className="border m-0 p-0"
                   style={{backgroundColor: UI_WHITE}}>
                 <div className="d-flex ms-2">
                     <Button variant="outline-primary"
-                            className="my-0 mx-1 p-0 border-0">
+                            className="my-0 mx-1 p-0 border-0"
+                            onClick={toggleTimer}>
                         <FaPlay size={25}/>
                     </Button>
                     <div className="my-0 mx-1 p-0 flex-grow-1">
                         <RangeSlider
                             value={time}
-                            min="1" max="52" step="1"
+                            min={1} max={52} step={1}
                             onChange={e => setTime(e.target.value)}
                         />
                     </div>
--- src/contexts/TimeContext.js
+++ src/contexts/TimeContext.js
@@ -1,13 +1,32 @@
-import React, {createContext, useContext, useMemo, useState} from 'react';
+import React, {createContext, useCallback, useContext, useMemo, useState} from 'react';
 
 const TimeContext = createContext();
 
 export function TimeProvider(props) {
-    const [time, setTime] = useState(24);
+    const [time, setTimeInternal] = useState(24);
+    const [intervalID, setIntervalID] = useState();
+
+    const setTime = useCallback((newTime) => {
+        if (newTime < 1 || newTime > 52) return;
+        setTimeInternal(newTime);
+    });
+
+    const incrementTime = useCallback(() => setTime(time => time < 52 ? time + 1 : 1), []);
+
+    const toggleTimer = useCallback(() => {
+        console.log("toggleTimer ")
+        if (intervalID) { //running
+            clearInterval(intervalID);
+            setIntervalID();
+        } else {
+            setIntervalID(setInterval(() => incrementTime(), 1500));
+        }
+    }, [intervalID, incrementTime]);
+
 
     const api = useMemo(() => ({
-        time, setTime
-    }), [time, setTime]);
+        time, setTime, toggleTimer
+    }), [time, setTime, toggleTimer]);
 
     return <TimeContext.Provider value={api}>
         {props.children}
