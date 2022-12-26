d91c7a0e3d626b1e7cdc43cb75365d8342b0e039
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-12-26 20:30:17 +0100

    layout

--- src/pages/GardenAreaListPage.js
+++ src/pages/GardenAreaListPage.js
@@ -1,10 +1,10 @@
 import {FaEye} from "react-icons/fa";
-import {IoMdAdd, IoMdRemove} from "react-icons/io";
+import {RxTriangleDown, RxTriangleRight} from "react-icons/rx";
 import {useGardenSelectorContext} from "../contexts/GardenSelectorContext";
 import {Button, Col, Container, Row} from "react-bootstrap";
 import {SHOW_AREA_ID, useControlsContext} from "../contexts/ControlsContext";
-import {ICON_SIZE_SMALL} from "../constants/uiSizes";
-import {useState} from "react";
+import {ICON_SIZE, ICON_SIZE_SMALL} from "../constants/uiSizes";
+import {useMemo, useState} from "react";
 
 function SmallButton(props) {
     const {onClick, children} = props;
@@ -20,7 +20,7 @@ function AreaInfo(props) {
     const {toggleControlInCollection} = useControlsContext();
 
     return (
-        <Row className=" ps-0 pe-2 m-1 bg-white">
+        <Row className="ms-1">
             <Col xs="1" className="">
                 <SmallButton onClick={() => toggleControlInCollection(SHOW_AREA_ID, area.id)}>
                     <FaEye size={ICON_SIZE_SMALL}/>
@@ -34,58 +34,71 @@ function AreaInfo(props) {
     );
 }
 
+function toggleAreas(plantWithAreas, getControlValue, setOneControl) {
+    const idsOfAreasToToggle = plantWithAreas.areas.map(a => a.id);
+    const currentAreaIdControls = getControlValue(SHOW_AREA_ID) ?? {};
+    const foundOneTrue = idsOfAreasToToggle.some(id => currentAreaIdControls[id]);
+    const toggledAreaIdControls = idsOfAreasToToggle.reduce((tempResult, k) => ({
+        ...tempResult, [k]: !foundOneTrue
+    }), {});
+    const newAreaIdControls = {...currentAreaIdControls, ...toggledAreaIdControls};
+    setOneControl(SHOW_AREA_ID, newAreaIdControls);
+}
+
 function PlantWithAreas(props) {
     const {plantWithAreas, showAllAreaInfos} = props;
     const {controlValue, setOneControl,} = useControlsContext();
 
-    function toggleAreas() {
-        const idsOfAreasToToggle = plantWithAreas.areas.map(a => a.id);
-        const currentAreaIdControls = controlValue(SHOW_AREA_ID) ?? {};
-        const foundOneTrue = idsOfAreasToToggle.some(id => currentAreaIdControls[id]);
-        const toggledAreaIdControls = idsOfAreasToToggle.reduce((tempResult, k) => ({
-            ...tempResult, [k]: !foundOneTrue
-        }), {});
-        const newAreaIdControls = {...currentAreaIdControls, ...toggledAreaIdControls};
-        setOneControl(SHOW_AREA_ID, newAreaIdControls);
-    }
-
     return (
-        <>
-            <Row className="m-2"
-                 style={{backgroundColor: plantWithAreas.plant.flowerColor}}>
-                <Col xs={1} className="m-1 me-0 bg-white">
+        <Container className=""
+                   style={{borderWidth: "3px", borderStyle: "solid", borderColor: plantWithAreas.plant.flowerColor}}>
+            <Row className="bg-white p-1">
+                <Col xs={1} className="">
                     <SmallButton
-                        onClick={() => toggleAreas()}>
+                        onClick={() => toggleAreas(plantWithAreas, controlValue, setOneControl)}>
                         <FaEye size={ICON_SIZE_SMALL}/>
                     </SmallButton>
                 </Col>
-                <Col xs={showAllAreaInfos ? 4 : ""} className="m-1 ms-0 bg-white">
+                <Col className="">
                     {plantWithAreas.plant.name}
                 </Col>
-                {showAllAreaInfos && <Col className="m-0">
-                    {plantWithAreas.areas.map(a => <AreaInfo key={a.id} area={a}/>)}
-                </Col>
+            </Row>
+            <Row className="bg-white p-1">
+                {showAllAreaInfos &&
+                    <Col className="">
+                        {plantWithAreas.areas.map(a => <AreaInfo key={a.id} area={a}/>)}
+                    </Col>
                 }
             </Row>
-        </>
+        </Container>
     );
 }
 
-function AreaInfoGroupedByPlant(props) {
-    const {showAllAreaInfos} = props;
-
-    const {areasSelectedGarden} = useGardenSelectorContext();
+function getPlantsWithAreas(areasSelectedGarden) {
     const plantNamesSelectedGarden = [...new Set(areasSelectedGarden.map(a => a.plantName))];
-    const plantsSelectedGardenWithAreas = plantNamesSelectedGarden.map(p => ({
+    return plantNamesSelectedGarden.map(p => ({
         plantName: p,
         plant: areasSelectedGarden.find(a => a.plantName === p).plant,
         areas: areasSelectedGarden.filter(a => a.plantName === p)
     }));
+}
+
+function AreaInfoGroupedByPlant(props) {
+    const {showAllAreaInfos} = props;
+    const {areasSelectedGarden} = useGardenSelectorContext();
+    const plantsWithAreas = useMemo(() => getPlantsWithAreas(areasSelectedGarden), [areasSelectedGarden]);
+
     return (
-        <>
-            {plantsSelectedGardenWithAreas.map(p => <PlantWithAreas key={p.plant.id} plantWithAreas={p}
-                                                                    showAllAreaInfos={showAllAreaInfos}/>)}
-        </>
+        <Container className="mx-auto">
+            <Row className="m-0">
+                {plantsWithAreas.map(p =>
+                    <Col xs={12} md={6} lg={4} xl={3} className="p-1">
+                        <PlantWithAreas key={p.plant.id} plantWithAreas={p}
+                                        showAllAreaInfos={showAllAreaInfos}/>
+                    </Col>
+                )}
+            </Row>
+        </Container>
     );
 }
 
@@ -94,10 +107,11 @@ export function GardenAreaListPage() {
 
     return (
         <Container>
-            <h3>plants:</h3>
-            <SmallButton onClick={() => setShowAllAreaInfos(showAllAreaInfos => !showAllAreaInfos)}>
-                {showAllAreaInfos ? <IoMdRemove size={ICON_SIZE_SMALL}/> : <IoMdAdd size={ICON_SIZE_SMALL}/>}
-            </SmallButton>
+            <h3 className="">plants:
+                <SmallButton onClick={() => setShowAllAreaInfos(showAllAreaInfos => !showAllAreaInfos)}>
+                    {showAllAreaInfos ? <RxTriangleDown size={ICON_SIZE}/> : <RxTriangleRight size={ICON_SIZE}/>}
+                </SmallButton>
+            </h3>
             <AreaInfoGroupedByPlant showAllAreaInfos={showAllAreaInfos}/>
         </Container>
     )
