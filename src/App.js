c242515b2cefd5de199a7ea393522497b518488f
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2023-01-07 10:41:47 +0100

    todos

--- src/App.js
+++ src/App.js
@@ -58,26 +58,24 @@ export default function App() {
 
 
 /*
-TODO
 -- issues
-* select new garden --> controls to show areas/plants are broken
-* logo
-* proptypes voor area en plant
-* alle afmetingen *100?
-* gardenarealist - meerdere cards/row is niet ok want cards zijn niet even hoog - zie https://www.npmjs.com/package/react-masonry-css
-* gardenarealist - buttons and text not on same line
-* gardenselectionpage: which garden is selected? - is it edited?
+TODO issue -- select new garden --> controls to show areas/plants are broken
+TODO issue -- logo
+OK issue -- proptypes voor area en plant
+TODO issue -- alle afmetingen *100?
+TODO issue -- gardenarealist - meerdere cards/row is niet ok want cards zijn niet even hoog - zie https://www.npmjs.com/package/react-masonry-css
+TODO issue -- gardenarealist - buttons and text not on same line
+TODO issue -- gardenselectionpage: which garden is selected? - is it edited?
 
 --- bigger stories
-* plant topview
-* catalog always show areas? -- catalog is simply special kind of garden (limited - no edits)
-* database / storage
-* tutorial
+TODO bigger stories -- plant topview
+TODO bigger stories -- catalog always show areas? -- catalog is simply special kind of garden (limited - no edits)
+TODO bigger stories -- database / storage
+TODO bigger stories -- tutorial
 
 -- easy - later
-* waaien
-* time runner: random zoom
-* color scheme
-* timer autorun fast/slow
-
+TODO easy - later -- waaien
+TODO easy - later -- time runner: random zoom
+TODO easy - later -- color scheme
+TODO easy - later -- timer autorun fast/slow
  */
