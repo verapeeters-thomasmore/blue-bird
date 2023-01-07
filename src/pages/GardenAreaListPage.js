9c2d3b9b1ffa8f1642266cbbabad3687cb72dc72
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2023-01-07 10:12:04 +0100

    add propTypes in own file

--- src/pages/GardenAreaListPage.js
+++ src/pages/GardenAreaListPage.js
@@ -6,6 +6,7 @@ import {GREEN} from "../constants/threeColors";
 import {PlantPicture} from "../components/PlantPicture";
 import {ExpandButton, EyeButton, FlowerButton, SmallButton} from "../components/SmallButtons";
 import PropTypes from "prop-types";
+import {plantDataPropType, plantWithAreasPropType} from "../types";
 
 
 function NumCol(props) {
@@ -34,35 +35,6 @@ function AreaInfo(props) {
 
 const GardenAreaListPageContext = createContext();
 const useGardenAreaListPageContext = () => useContext(GardenAreaListPageContext);
-
-const plantDataPropType = PropTypes.shape({
-        id: PropTypes.number,
-        shortName: PropTypes.string,
-        textureWithoutFlower: PropTypes.string,
-        textureWithFlower: PropTypes.string,
-        flowerColor: PropTypes.string,
-        maxHeight: PropTypes.number,
-        plantsPerM2: PropTypes.number,
-        timeLine: PropTypes.shape({
-            growStart: PropTypes.number,
-            growFinal: PropTypes.number,
-            FlowerStart: PropTypes.number,
-            flowerEnd: PropTypes.number,
-            die: PropTypes.number,
-        }),
-    }
-);
-const areaPropType = PropTypes.shape({
-    id: PropTypes.number,
-    x: PropTypes.number,
-    z: PropTypes.number,
-    plantName: PropTypes.string,
-});
-const plantWithAreasPropType = PropTypes.shape({
-    plantName: PropTypes.string,
-    plant: plantDataPropType,
-    areas: PropTypes.arrayOf(areaPropType)
-});
 
 function PlantFiche(props) {
     const {plantWithAreas, showPlantInfo} = props;
---
+++ src/types.js
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+
+export const plantDataPropType = PropTypes.shape({
+        id: PropTypes.number,
+        shortName: PropTypes.string,
+        textureWithoutFlower: PropTypes.string,
+        textureWithFlower: PropTypes.string,
+        flowerColor: PropTypes.string,
+        maxHeight: PropTypes.number,
+        plantsPerM2: PropTypes.number,
+        timeLine: PropTypes.shape({
+            growStart: PropTypes.number,
+            growFinal: PropTypes.number,
+            FlowerStart: PropTypes.number,
+            flowerEnd: PropTypes.number,
+            die: PropTypes.number,
+        }),
+    }
+);
+
+const areaPropType = PropTypes.shape({
+    id: PropTypes.number,
+    x: PropTypes.number,
+    z: PropTypes.number,
+    plantName: PropTypes.string,
+});
+
+export const plantWithAreasPropType = PropTypes.shape({
+    plantName: PropTypes.string,
+    plant: plantDataPropType,
+    areas: PropTypes.arrayOf(areaPropType)
+});
